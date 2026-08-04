ee07c1f31bf288c3700839b1ced82f4a56ad0a87
vxjnc <vxjnc0@gmail.com>
2026-08-04 09:38:05 +0400

    perf: full delay-line for next-row error diffusion, eliminating memory writes

--- src/core/error_diffusion.hpp
+++ src/core/error_diffusion.hpp
@@ -31,8 +31,11 @@ public:
 
     void beginRow(int y) {
         m_err.swapAndResetNext();
-        m_carry[0] = 0;
-        m_carry[1] = 0;
+        for (int i = 0; i < kChannels; ++i) {
+            m_carry[i] = 0;
+            m_delayNear[i] = 0;
+            m_delayFar[i] = 0;
+        }
     }
 
     template <ScanDirection Dir> LA8 ditherRgbToIndex2D(int x, int y) {
@@ -62,20 +65,33 @@ public:
             int* RESTRICT next_row = m_err.next[i].data();
 
             if constexpr (Dir == ScanDirection::Reverse) {
-                next_row[x + 2] += b;
-                next_row[x + 1] += c;
-                next_row[x] += d;
+                next_row[x + 2] += m_delayFar[i] + b;
             }
             else {
-                next_row[x] += b;
-                next_row[x + 1] += c;
-                next_row[x + 2] += d;
+                next_row[x] += m_delayFar[i] + b;
             }
+            m_delayFar[i] = m_delayNear[i] + c;
+            m_delayNear[i] = d;
 
             m_carry[i] = a;
         }
 
         return getPaletteColor(index);
+    }
+
+    template <ScanDirection Dir> void flushRowTail(int width) {
+        for (int i = 0; i < kChannels; ++i) {
+            int* RESTRICT next_row = m_err.next[i].data();
+
+            if constexpr (Dir == ScanDirection::Reverse) {
+                next_row[1] += m_delayFar[i];
+                next_row[0] += m_delayNear[i];
+            }
+            else {
+                next_row[width] += m_delayFar[i];
+                next_row[width + 1] += m_delayNear[i];
+            }
+        }
     }
 
     void dither_image_to_indexed(double factor, const LA8* RESTRICT srcData, int width, int height,
@@ -89,6 +105,7 @@ public:
             for (int x = 0; x < width; ++x) {
                 dstData[y * width + x] = ditherRgbToIndex2D<ScanDirection::Forward>(x, y);
             }
+            flushRowTail<ScanDirection::Forward>(width);
 
             // --- 2. Odd row (y + 1) -> Reverse scan ---
             ++y;
@@ -96,6 +113,7 @@ public:
             for (int x = width - 1; x >= 0; --x) {
                 dstData[y * width + x] = ditherRgbToIndex2D<ScanDirection::Reverse>(x, y);
             }
+            flushRowTail<ScanDirection::Reverse>(width);
         }
 
         // Tail: handle the remaining (even) row if height is odd
@@ -104,6 +122,7 @@ public:
             for (int x = 0; x < width; ++x) {
                 dstData[y * width + x] = ditherRgbToIndex2D<ScanDirection::Forward>(x, y);
             }
+            flushRowTail<ScanDirection::Forward>(width);
         }
     }
 
@@ -116,4 +135,6 @@ private:
     int m_factor = 0;
 
     int m_carry[kChannels] = {0, 0};
+    int m_delayNear[kChannels] = {0, 0};
+    int m_delayFar[kChannels] = {0, 0};
 };
