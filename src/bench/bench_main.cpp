3347297882bb89f7f39bc03178a6f4513c3d2f73
vxjnc <vxjnc0@gmail.com>
2026-08-05 06:56:18 +0400

    test: add benchmarks for OldDithering and OrderedDithering

--- src/bench/bench_main.cpp
+++ src/bench/bench_main.cpp
@@ -5,14 +5,16 @@
 
 #include "core/error_diffusion.hpp"
 #include "core/la8.hpp"
+#include "core/old_dithering.hpp"
+#include "core/ordered_dithering.hpp"
 
-class ErrorDiffusionBenchmark : public benchmark::Fixture {
+template <typename DitherT> class DitherBenchmark : public benchmark::Fixture {
 public:
     int width = 0;
     int height = 0;
     std::vector<LA8> src_pixels;
     std::vector<LA8> dst_pixels;
-    ErrorDiffusionDither dither;
+    DitherT dither;
 
     void SetUp(const ::benchmark::State& state) override {
         const int dim = static_cast<int>(state.range(0));
@@ -36,6 +38,10 @@ public:
     }
 };
 
+using ErrorDiffusionBenchmark = DitherBenchmark<ErrorDiffusionDither>;
+using OldDitheringBenchmark = DitherBenchmark<OldDithering>;
+using OrderedDitheringBenchmark = DitherBenchmark<OrderedDithering>;
+
 BENCHMARK_DEFINE_F(ErrorDiffusionBenchmark, Process)(benchmark::State& state) {
     for (auto _ : state) {
         dither.dither_image_to_indexed(1.0, src_pixels.data(), width, height, dst_pixels.data());
@@ -50,7 +56,45 @@ BENCHMARK_DEFINE_F(ErrorDiffusionBenchmark, Process)(benchmark::State& state) {
     state.counters["Dimension"] = static_cast<double>(width);
 }
 
+BENCHMARK_DEFINE_F(OldDitheringBenchmark, Process)(benchmark::State& state) {
+    for (auto _ : state) {
+        dither.dither_image_to_indexed(1.0, src_pixels.data(), width, height, dst_pixels.data());
+
+        benchmark::DoNotOptimize(src_pixels.data());
+        benchmark::DoNotOptimize(dst_pixels.data());
+        benchmark::ClobberMemory();
+    }
+
+    state.SetItemsProcessed(state.iterations() * width * height);
+    state.SetBytesProcessed(state.iterations() * width * height * sizeof(LA8));
+    state.counters["Dimension"] = static_cast<double>(width);
+}
+
+BENCHMARK_DEFINE_F(OrderedDitheringBenchmark, Process)(benchmark::State& state) {
+    for (auto _ : state) {
+        dither.dither_image_to_indexed(1.0, src_pixels.data(), width, height, dst_pixels.data());
+
+        benchmark::DoNotOptimize(src_pixels.data());
+        benchmark::DoNotOptimize(dst_pixels.data());
+        benchmark::ClobberMemory();
+    }
+
+    state.SetItemsProcessed(state.iterations() * width * height);
+    state.SetBytesProcessed(state.iterations() * width * height * sizeof(LA8));
+    state.counters["Dimension"] = static_cast<double>(width);
+}
+
 BENCHMARK_REGISTER_F(ErrorDiffusionBenchmark, Process)
+    ->Unit(benchmark::kNanosecond)
+    ->RangeMultiplier(2)
+    ->Range(64, 2048);
+
+BENCHMARK_REGISTER_F(OldDitheringBenchmark, Process)
+    ->Unit(benchmark::kNanosecond)
+    ->RangeMultiplier(2)
+    ->Range(64, 2048);
+
+BENCHMARK_REGISTER_F(OrderedDitheringBenchmark, Process)
     ->Unit(benchmark::kNanosecond)
     ->RangeMultiplier(2)
     ->Range(64, 2048);
